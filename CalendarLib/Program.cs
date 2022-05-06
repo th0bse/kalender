7ea93e39e2041e61a34125bd3a82c1b0b52b10de
Thorben von Rekowski <thorben.von-rekowski@gwdg.de>
2022-05-06 08:48:45 +0200

    feat: add separate class for Weekday enum
    Move the Weekday enum in a separate file to keep the main library class
    clean and readable.
    
    Add extension methods to the Weekday enum for conversion between string
    literals, int literals and enum instances.
    
    Author: Thorben von Rekowski <thorben.von-rekowski@gwdg.de>

--- CalendarLib/Program.cs
+++ CalendarLib/Program.cs
@@ -34,92 +34,30 @@ namespace CalendarLib
             year %= 100;
             return ((26 * month - 1) / 10 + day + year + year / 4 + c / 4 + 5 * c) % 7;
         }
-    }
 
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
-    public enum Weekday
-    {
-        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
-    }
-
-    public static class WeekdayExtensions
-    {
-        /// <summary>
-        /// Extension method to get the corresponding integer literal for
-        /// a given day of the week. (e.g. 0 for sunday, 1 for monday, etc.)
-        ///
-        /// The numbering follows the conventions described in RFC3339.
-        /// </summary>
-        /// <param name="weekday">the day of the week</param>
-        /// <returns>the corresponding integer literal</returns>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public static int IntLiteral(this Weekday weekday) => weekday switch
+        public static Weekday GetWeekdayForDate(int day, int month, int year)
         {
-            Weekday.SUNDAY => 0,
-            Weekday.MONDAY => 1,
-            Weekday.TUESDAY => 2,
-            Weekday.WEDNESDAY => 3,
-            Weekday.THURSDAY => 4,
-            Weekday.FRIDAY => 5,
-            Weekday.SATURDAY => 6,
-            _ => throw new ArgumentOutOfRangeException(nameof(weekday), weekday, null)
-        };
+            return ZellerCongruence(day, month, year).ToWeekday();
+        }
 
-        /// <summary>
-        /// Extension method to get the corresponding string literal for
-        /// a given day of the week.
-        /// </summary>
-        /// <param name="weekday">the day of the week</param>
-        /// <returns>the corresponding string literal</returns>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public static string StringLiteral(this Weekday weekday) => weekday switch
+        public static Weekday GetWeekdayForFirstOfMonth(Month month, int year)
         {
-            Weekday.SUNDAY => "Sunday",
-            Weekday.MONDAY => "Monday",
-            Weekday.TUESDAY => "Tuesday",
-            Weekday.WEDNESDAY => "Wednesday",
-            Weekday.THURSDAY => "Thursday",
-            Weekday.FRIDAY => "Friday",
-            Weekday.SATURDAY => "Saturday",
-            _ => throw new ArgumentOutOfRangeException(nameof(weekday), weekday, null)
-        };
+            return ZellerCongruence(1, month.IntLiteral(), year).ToWeekday();
+        }
 
-        /// <summary>
-        /// Extension method to get the corresponding Weekday instance for
-        /// a given integer literal.
-        /// </summary>
-        /// <param name="intLiteral">the integer literal</param>
-        /// <returns>the corresponding Weekday instance</returns>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public static Weekday ToWeekday(this int intLiteral) => intLiteral switch
+        public static bool IsLeapYear(int year)
         {
-            0 => Weekday.SUNDAY,
-            1 => Weekday.MONDAY,
-            2 => Weekday.TUESDAY,
-            3 => Weekday.WEDNESDAY,
-            4 => Weekday.THURSDAY,
-            5 => Weekday.FRIDAY,
-            6 => Weekday.SATURDAY,
-            _ => throw new ArgumentOutOfRangeException(nameof(intLiteral), intLiteral, null)
-        };
+            var isLeapYear = false;
+            if (year % 4 == 0)
+            {
+                isLeapYear = true;
+                if (year % 100 == 0)
+                {
+                    isLeapYear = false || year % 400 == 0;
+                }
+            }
 
-        /// <summary>
-        /// Extension method to get the corresponding Weekday instance for
-        /// a given string literal.
-        /// </summary>
-        /// <param name="stringLiteral">the string literal</param>
-        /// <returns>the corresponding Weekday instance</returns>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        public static Weekday ToWeekday(this string stringLiteral) => stringLiteral.ToLower() switch
-        {
-            "sunday" => Weekday.SUNDAY,
-            "monday" => Weekday.MONDAY,
-            "tuesday" => Weekday.TUESDAY,
-            "wednesday" => Weekday.WEDNESDAY,
-            "thursday" => Weekday.THURSDAY,
-            "friday" => Weekday.FRIDAY,
-            "saturday" => Weekday.SATURDAY,
-            _ => throw new ArgumentOutOfRangeException(nameof(stringLiteral), stringLiteral, null)
-        };
+            return isLeapYear;
+        }
     }
 }
