741fdb1f090dc4fcd34cffc400d16e9efda01424
Thorben von Rekowski <thorben.von-rekowski@gwdg.de>
2022-05-06 11:39:13 +0200

    feat: loop until user exits
    Author: Thorben von Rekowski <thorben.von-rekowski@gwdg.de>

--- CalendarCLI/Program.cs
+++ CalendarCLI/Program.cs
@@ -5,24 +5,34 @@ namespace CalendarCLI
 {
     public class CalendarCLI
     {
+
         public static void Main(string[] args)
         {
-            // get month and year to print
-            int month;
-            do
+
+            Console.WriteLine("To terminate the program, enter \"exit\" at any time");
+            while (true)
             {
-                Console.Write("Please enter the month you want to display:");
-            } while (!int.TryParse(Console.ReadLine(), out month));
+                int month;
+                string s;
+                do
+                {
+                    Console.Write("Please enter the month you want to display:");
+                    s = Console.ReadLine();
+                    if (s.Equals("exit")) Environment.Exit(0);
+                } while (!int.TryParse(s, out month));
 
-            Month monthToDisplay = month.ToMonth();
+                Month monthToDisplay = month.ToMonth();
 
-            int yearToDisplay;
-            do
-            {
-                Console.Write("Please enter the year you want to display:");
-            } while (!int.TryParse(Console.ReadLine(), out yearToDisplay));
+                int yearToDisplay;
+                do
+                {
+                    Console.Write("Please enter the year you want to display:");
+                    s = Console.ReadLine();
+                    if (s.Equals("exit")) Environment.Exit(0);
+                } while (!int.TryParse(s, out yearToDisplay));
 
-            DisplayMonth(monthToDisplay, yearToDisplay);
+                DisplayMonth(monthToDisplay, yearToDisplay);
+            }
         }
 
         private static void DisplayMonth(Month month, int year)
